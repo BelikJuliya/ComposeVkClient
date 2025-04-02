ac08acf7e3c54dca52e6bbf4904f58f76ba96d15
«Yuliya <YBelik@renins.com>
2025-04-02 08:30:31 +0300

    [Lesson 15] Swipe animation

--- app/src/main/java/com/example/composeapp/ui/vk/VkNewsMainScareen.kt
+++ app/src/main/java/com/example/composeapp/ui/vk/VkNewsMainScareen.kt
@@ -1,5 +1,11 @@
 package com.example.composeapp.ui.vk
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,9 +25,13 @@ import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -79,54 +89,77 @@ fun MainScreen(viewModel: MainViewModel) {
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 state = listState
             ) {
-                items(items = postsList.value, key = { it.postId }
+                items(
+                    items = postsList.value,
+                    key = { it.postId }
                 ) { model ->
+                    // Состояние видимости
+                    var isVisible by remember { mutableStateOf(true) }
+
                     val dismissState = rememberSwipeToDismissBoxState(
                         confirmValueChange = { dismissValue ->
-                            if (dismissValue != SwipeToDismissBoxValue.EndToStart) { //  Проверяем, что свайп был выполнен
-                                viewModel.deleteItem(model)
-                                true  // Разрешаем анимацию удаления
+                            if (dismissValue == SwipeToDismissBoxValue.EndToStart) {
+                                isVisible = false // Скрываем элемент
+                                true
                             } else {
-                                false // Отменяем удаление
+                                false
                             }
                         }
                     )
-                    SwipeToDismissBox(
-                        state = dismissState,
-                        enableDismissFromStartToEnd = false,
-                        backgroundContent = {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(8.dp)
-                                    .background(Color.Red.copy(alpha = 0.5f))
-                            ) {
-                                Text(
-                                    modifier = Modifier
-                                        .padding(16.dp),
-                                    text = "Delete item",
-                                    color = MaterialTheme.colorScheme.onBackground,
-                                    fontSize = 24.sp
-                                )
+
+                    // Управляем анимацией через MutableTransitionState
+                    val transitionState = remember { MutableTransitionState(true) }
+                    transitionState.targetState = isVisible
+
+                    AnimatedVisibility(
+                        visibleState = transitionState,
+                        enter = fadeIn() + slideInHorizontally(initialOffsetX = { it }),
+                        exit = fadeOut() + slideOutHorizontally(targetOffsetX = { -it })
+                    ) {
+                        // Ждём завершения анимации
+                        LaunchedEffect(transitionState.isIdle && !transitionState.currentState) {
+                            // Если элемент исчез
+                            if (transitionState.isIdle && !transitionState.currentState) {
+                                viewModel.deleteItem(model)
                             }
                         }
-                    ) {
-                        PostCard(
-                            modifier = Modifier.padding(padding),
-                            feedPost = model,
-                            onLikeClickListener = { statistics, feedPost ->
-                                viewModel.updateCount(statistic = statistics, model = feedPost)
-                            },
-                            onShareClickListener = { statistics, feedPost ->
-                                viewModel.updateCount(statistic = statistics, model = feedPost)
-                            },
-                            onCommentClickListener = { statistics, feedPost ->
-                                viewModel.updateCount(statistic = statistics, model = feedPost)
-                            },
-                            onViewClickListener = { statistics, feedPost ->
-                                viewModel.updateCount(statistic = statistics, model = feedPost)
+
+                        SwipeToDismissBox(
+                            state = dismissState,
+                            enableDismissFromStartToEnd = false,
+                            backgroundContent = {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(8.dp)
+                                        .background(Color.Red.copy(alpha = 0.5f))
+                                ) {
+                                    Text(
+                                        modifier = Modifier.padding(16.dp),
+                                        text = "Delete item",
+                                        color = MaterialTheme.colorScheme.onBackground,
+                                        fontSize = 24.sp
+                                    )
+                                }
                             }
-                        )
+                        ) {
+                            PostCard(
+                                modifier = Modifier.padding(padding),
+                                feedPost = model,
+                                onLikeClickListener = { statistics, feedPost ->
+                                    viewModel.updateCount(statistic = statistics, model = feedPost)
+                                },
+                                onShareClickListener = { statistics, feedPost ->
+                                    viewModel.updateCount(statistic = statistics, model = feedPost)
+                                },
+                                onCommentClickListener = { statistics, feedPost ->
+                                    viewModel.updateCount(statistic = statistics, model = feedPost)
+                                },
+                                onViewClickListener = { statistics, feedPost ->
+                                    viewModel.updateCount(statistic = statistics, model = feedPost)
+                                }
+                            )
+                        }
                     }
                 }
             }
