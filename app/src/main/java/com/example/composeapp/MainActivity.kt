052fff7cde83ceeca12d9730e996554a16165064
«Yuliya <YBelik@renins.com>
2025-04-07 18:06:01 +0300

    [Block-7-lesson-1] VK login

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.composeapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
@@ -16,14 +17,34 @@ import com.example.composeapp.ui.vk.MainScreen
 import com.example.composeapp.ui.theme.ComposeAppTheme
 import com.example.composeapp.ui.vk.news.NewsFeedViewModel
 import com.example.composeapp.ui.vk.comments.CommentsViewModel
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             ComposeAppTheme {
+                rememberLauncherForActivityResult(contract = VK.getVKAuthActivityResultContract()) {
+                    val authLauncher = VK.login(this) { result: VKAuthenticationResult ->
+                        when (result) {
+                            is VKAuthenticationResult.Success -> {
+                                // User passed authorization
+                            }
+
+                            is VKAuthenticationResult.Failed -> {
+                                // User didn't pass authorization
+                            }
+                        }
+                    }
+                    authLauncher.launch(arrayListOf(VKScope.WALL))
+
+                }
+
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
---
+++ app/src/main/java/com/example/composeapp/ui/vk/comments/ActivityResultTest.kt
@@ -0,0 +1,26 @@
+package com.example.composeapp.ui.vk.comments
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
+
+@Composable
+fun ActivityResultTest() {
+    var imageUri by remember {
+        mutableStateOf(Uri.EMPTY)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = {
+            imageUri = it
+        }
+    )
+}
